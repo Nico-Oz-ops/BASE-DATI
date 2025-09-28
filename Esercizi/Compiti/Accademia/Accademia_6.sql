8dd21ae2289ad9e22304f3762ddcf56d455ddadd
Nico Valenzuela <nicolasvr1988@gmail.com>
2025-09-28 15:23:27 +0200

    Update 7

--- Esercizi/Compiti/Accademia/Accademia_6.sql
+++ Esercizi/Compiti/Accademia/Accademia_6.sql
@@ -1,18 +1,18 @@
 /*1. Quanti sono gli strutturati di ogni fascia?*/
 SELECT posizione, COUNT(*) as numero
 FROM persona
-GROUP BY posizione
+GROUP BY posizione;
 
 /*2. Quanti sono gli strutturati con stipendio ≥ 40000?*/
 SELECT COUNT(*) as numero
 FROM persona
-WHERE stipendio >= 40000
+WHERE stipendio >= 40000;
 
 /*3. Quanti sono i progetti già finiti che superano il budget di 50000?*/
 SELECT COUNT(*) as numero
 FROM progetto
-WHERE budget >= 50000
-AND CURRENT_DATE > fine
+WHERE budget > 50000
+AND CURRENT_DATE > fine;
 
 /*4. Qual è la media, il massimo e il minimo delle ore delle attività relative al progetto
 ‘Pegasus’ ?*/
@@ -33,16 +33,16 @@ FROM attivitaprogetto ap
 JOIN progetto p ON ap.progetto = p.id
 JOIN persona per ON ap.persona = per.id
 WHERE p.nome = 'Pegasus'
-GROUP BY per.id
-ORDER BY media DESC
+GROUP BY per.id, per.nome, per.cognome
+ORDER BY media DESC;
 
 /*6. Qual è il numero totale di ore dedicate alla didattica da ogni docente?*/
 SELECT p.id as id_persona, p.nome, p.cognome, SUM(anp.oreDurata) as ore_didattica
 FROM attivitanonprogettuale anp
 JOIN persona p ON anp.persona = p.id
 WHERE anp.tipo = 'Didattica'
-GROUP BY p.id
-ORDER BY p.id ASC
+GROUP BY p.id, p.nome, p.cognome
+ORDER BY p.id ASC;
 
 /*7. Qual è la media, il massimo e il minimo degli stipendi dei ricercatori?*/
 SELECT 
@@ -50,7 +50,7 @@ SELECT
 	MIN(stipendio) as minimo,
 	MAX(stipendio) as massimo
 FROM persona
-WHERE posizione = 'Ricercatore'
+WHERE posizione = 'Ricercatore';
 
 /*8. Quali sono le medie, i massimi e i minimi degli stipendi dei ricercatori, dei professori
 associati e dei professori ordinari?*/
@@ -59,7 +59,7 @@ SELECT posizione,
 	MIN(stipendio) as minimo,
 	MAX(stipendio) as massimo
 FROM persona
-GROUP BY posizione
+GROUP BY posizione;
 
 /*9. Quante ore ‘Ginevra Riva’ ha dedicato ad ogni progetto nel quale ha lavorato?*/
 SELECT pr.id as id_progetto, pr.nome, SUM(ap.oreDurata) as totale_ore
@@ -67,20 +67,25 @@ FROM attivitaprogetto ap
 JOIN persona p ON ap.persona = p.id
 JOIN progetto pr ON ap.progetto = pr.id
 WHERE p.nome = 'Ginevra' AND p.cognome = 'Riva'
-GROUP BY pr.id
-
+GROUP BY pr.id, pr.nome;
 
 /*10. Qual è il nome dei progetti su cui lavorano più di due strutturati?*/
 SELECT pr.id as id_progetto, pr.nome as progetto
 FROM attivitaprogetto ap
 JOIN progetto pr ON ap.progetto = pr.id
-GROUP BY pr.id
-HAVING COUNT(DISTINCT ap.persona) > 2
+GROUP BY pr.id, pr.nome
+HAVING COUNT(DISTINCT ap.persona) > 2;
 
 /*11. Quali sono i professori associati che hanno lavorato su più di un progetto?*/
-SELECT p.id, p.nome, p.cognome
+SELECT p.id as id_professore, p.nome, p.cognome
 FROM persona p
 JOIN attivitaprogetto ap ON ap.persona = p.id
 WHERE p.posizione = 'Professore Associato'
-GROUP BY p.id
-HAVING COUNT(DISTINCT ap.progetto) > 1
+GROUP BY p.id, p.nome, p.cognome
+HAVING COUNT(DISTINCT ap.progetto) > 1;
+
+
+
+
+
+
--- Esercizi/Compiti/Cielo/Cielo_2.sql
+++ Esercizi/Compiti/Cielo/Cielo_2.sql
@@ -1,55 +1,89 @@
 /*1. Quante sono le compagnie che operano (sia in arrivo che in partenza) nei diversi
 aeroporti?*/
-
-
+SELECT a.codice, a.nome, COUNT(DISTINCT ap.comp) as num_compagnie
+FROM aeroporto a
+JOIN arrpart ap ON ap.arrivo = a.codice OR ap.partenza = a.codice
+GROUP BY a.codice, a.nome;
 
 /*2. Quanti sono i voli che partono dall’aeroporto ‘HTR’ e hanno una durata di almeno
 100 minuti?*/
-
-
+SELECT COUNT(*) as num_voli
+FROM volo v
+JOIN arrpart ap ON v.codice = ap.codice AND v.comp = ap.comp
+WHERE ap.partenza = 'HTR' AND v.durataMinuti >= 100;
 
 /*3. Quanti sono gli aeroporti sui quali opera la compagnia ‘Apitalia’, per ogni nazione
 nella quale opera?*/
-
-
+SELECT la.nazione, COUNT(DISTINCT a.codice) as num_aeroporto
+FROM aeroporto a
+JOIN luogoaeroporto la ON a.codice = la.aeroporto
+JOIN arrpart ap ON a.codice = ap.partenza OR a.codice = ap.arrivo
+WHERE ap.comp = 'Apitalia'
+GROUP BY la.nazione;
 
 /*4. Qual è la media, il massimo e il minimo della durata dei voli effettuati dalla
 compagnia ‘MagicFly’ ?*/
-
-
+SELECT 
+	AVG(durataminuti) as media, 
+	MIN(durataminuti) as minimo,
+	MAX(durataminuti) as massimo
+FROM volo
+WHERE comp = 'MagicFly';
 
 /*5. Qual è l’anno di fondazione della compagnia più vecchia che opera in ognuno degli
 aeroporti?*/
-
-
+SELECT a.codice, a.nome, MIN(c.annoFondaz) as anno
+FROM aeroporto a
+JOIN arrpart ap ON a.codice = ap.arrivo OR a.codice = ap.partenza
+JOIN compagnia c ON c.nome = ap.comp
+GROUP BY a.codice, a.nome;
 
 /*6. Quante sono le nazioni (diverse) raggiungibili da ogni nazione tramite uno o più
 voli?*/
-
-
+SELECT la1.nazione, COUNT(DISTINCT la2.nazione) as raggiungibili
+FROM arrpart ap
+JOIN luogoaeroporto la1 ON ap.partenza = la1.aeroporto
+JOIN luogoaeroporto la2 ON ap.arrivo = la2.aeroporto
+GROUP BY la1.nazione;
 
 /*7. Qual è la durata media dei voli che partono da ognuno degli aeroporti?*/
-
-
+SELECT a.codice, a.nome, AVG(v.durataminuti) as media_durata
+FROM aeroporto a
+JOIN arrpart ap ON a.codice = ap.partenza 
+JOIN volo v ON v.codice = ap.codice AND v.comp = ap.comp
+GROUP BY a.codice, a.nome;
 
 /*8. Qual è la durata complessiva dei voli operati da ognuna delle compagnie fondate
 a partire dal 1950?*/
-
-
+SELECT c.nome, SUM(v.durataMinuti) as durata_totale
+FROM volo v
+JOIN compagnia c ON v.comp = c.nome
+WHERE c.annoFondaz >= 1950
+GROUP BY c.nome;
 
 /*9. Quali sono gli aeroporti nei quali operano esattamente due compagnie?*/
-
-
+SELECT a.codice, a.nome
+FROM aeroporto a
+JOIN arrpart ap ON a.codice = ap.partenza OR a.codice = ap.arrivo
+GROUP BY a.codice, a.nome
+HAVING COUNT(DISTINCT ap.comp) = 2;
 
 /*10. Quali sono le città con almeno due aeroporti?*/
-
-
+SELECT citta
+FROM luogoaeroporto
+GROUP BY citta
+HAVING COUNT(aeroporto) >= 2;
 
 /*11. Qual è il nome delle compagnie i cui voli hanno una durata media maggiore di 6
 ore?*/
-
-
-
+SELECT comp as compagnia
+FROM volo
+GROUP BY comp
+HAVING AVG(durataminuti) > 360;
 
 /*12. Qual è il nome delle compagnie i cui voli hanno tutti una durata maggiore di 100
 minuti?*/
+SELECT comp as compagnia
+FROM volo
+GROUP BY comp
+HAVING MIN(durataminuti) > 100;
